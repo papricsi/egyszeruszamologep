8183d2316c3adf6d1160de1f8a5d5eaa8b98d667
papricsi <44347955+papricsi@users.noreply.github.com>
2018-10-26 21:43:18 +0200

    Add files via upload

---
+++ szamologep.py
@@ -0,0 +1,40 @@
+print ('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+print('Üdvözöllek az egyszeru számológépben!                                              Ver. 1.0')
+print ('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+print ('Help: Eloszor add meg, hogy mit szeretnel(Osszeadas, Kivonas, Szorzas, Osztas, Hatvanyozas ) \n majd add meg a tagokat(Ha nincs tobb tag,akkor irj 0 -t! ')
+print ('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+print('')
+
+
+All= raw_input ('Osszeadas(1), Kivonas(2), Szorzas(3), Osztas(4), Hatvanyozas(5)')
+All2=0
+
+
+num1 = int (input('Add meg az elso tagot'))
+num2 = int (input('Add meg a második tagot'))
+
+
+if All == "1":
+    All2 = int(num1) + int(num2)
+    print (num1, '+' , num2,'=', All2)
+    
+
+if All == "2":
+    All2 = int(num1) - int(num2)
+    print (num1, '-' , num2,'=', All2)
+     
+    
+
+if All == "3":
+    All2 = int(num1) * int(num2)
+    print (num1, '*' , num2,'=', All2)
+
+if All == "4":
+    All2 = int(num1) / int(num2)
+    print (num1, '/' , num2,'=', All2) 
+
+if All == "5":
+    All2 = int(num1) ** int(num2)
+    print (num1, '**' , num2,'=', All2) 
+    
+print ('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
